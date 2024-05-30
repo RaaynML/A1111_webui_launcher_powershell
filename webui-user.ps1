4611347fad896e4b9df6bb7f94baa242bef0e79a
Rayn <9046104+RaaynML@users.noreply.github.com>
2024-05-30 02:59:03 -0700

    Create webui-user.ps1
    webui-user Powershell version

---
+++ webui-user.ps1
@@ -0,0 +1,12 @@
+Set-PSDebug Off;
+
+$env:PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.6,max_split_size_mb:512
+$env:SD_WEBUI_LOG_LEVEL=ERROR
+
+$env:PYTHON=
+$env:GIT=
+$env:VENV_DIR=.\venv
+
+$env:COMMANDLINE_ARGS=--theme dark --disable-nan-check --unload-gfpgan --opt-channelslast --disable-console-progressbars --sub-quad-chunk-threshold 80
+
+call webui.ps1
