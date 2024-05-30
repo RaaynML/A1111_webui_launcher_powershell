430766e41ae6a3f6a0ddd714d81890176ba11039
Rayn <9046104+RaaynML@users.noreply.github.com>
2024-05-30 03:21:07 -0700

    Slim version of webui-user.ps1
    Just use this and skip all the checks after installation

--- webui-user.ps1
+++ webui-user.ps1
@@ -1,12 +1,9 @@
 Set-PSDebug -Off;
 
-$host.ui.RawUI.WindowTitle = "A1111-webui"
+$host.ui.RawUI.WindowTitle = "A1111-webui";
 $env:PYTORCH_CUDA_ALLOC_CONF="garbage_collection_threshold:0.6,max_split_size_mb:512"
-$env:SD_WEBUI_LOG_LEVEL="ERROR"
 
-#$env:PYTHON=
-#$env:GIT=
-$env:VENV_DIR=".\venv"
-$env:COMMANDLINE_ARGS="--theme dark --disable-nan-check --unload-gfpgan --opt-channelslast --disable-console-progressbars --sub-quad-chunk-threshold 80"
+.\venv\Scripts\Activate.ps1;
+#Start-Sleep -s 1
 
-.\webui.ps1
+python launch.py --no-download-sd-model --opt-channelslast --unload-gfpgan --disable-nan-check --sub-quad-q-chunk-size 512 --sub-quad-kv-chunk-size 512 --sub-quad-chunk-threshold 80
